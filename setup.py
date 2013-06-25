d74b523c250390ad3ea0027a1ead28d60d912f54
Peter Lamut <peter.lamut@niteoweb.com>
2013-06-25 16:13:50 +0200

    Add Plone to buildout config

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ setup(
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
     install_requires=[
-        'docutils>=0.3',
+        #'docutils>=0.3',
+        'Plone'
     ],
 
     # metadata for upload to PyPI
