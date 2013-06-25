ac59a908ad9c36c241606b682b466c8eda79fe58
Peter Lamut <peter.lamut@niteoweb.com>
2013-06-25 23:20:09 +0200

    Make plamut a namespace package + some gardening

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
 """Installer for the plamut.odpustki package."""
 
+from setuptools import find_packages
 from setuptools import setup
+
 
 setup(
     name='Odpustki',
     version='0.1',
 
     # list of packages to process during setup
-    packages=['plamut.odpustki'],
+    packages=find_packages('src'),
+    namespace_packages=['plamut'],
 
     # keys: package names (empty name stands for root package)
     # values: directory names relative to distribution root where packages
     #         (denoted by <key>) reside
     # ---> meaning: "all packages (=empty string) reside in the src dir"
     package_dir = {'': 'src'},
+
+    # XXX: .git files excluded this way? need to use exclude_package_data?
     include_package_data=True,
+    zip_safe=True,  # XXX: set to False?
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
